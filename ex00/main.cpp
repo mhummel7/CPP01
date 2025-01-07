8e7821b802ba1840f96cb73b7d00884ae1afa294
Manuel <mhummel@student.42heilbronn.de>
2025-01-07 10:24:15 +0100

    Create main.cpp
    Added a main which does both, stack allocation and heap allocation

---
+++ ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "Zombies.hpp"
+
+int main()
+{
+	// Stack allocation
+	std::cout << "Creating zombie on stack:" << std::endl;
+	randomChump("Stack Zombie");
+
+	// Heap allocation
+	std::cout << "\nCreating zombie on heap:" << std::endl;
+	Zombie *heapZombie = newZombie("Heap Zombie");
+	heapZombie->announce();
+	delete heapZombie;
+
+	return 0;
+}
