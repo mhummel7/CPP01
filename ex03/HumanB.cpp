34a8a035b4b9b18f466ea6673d6dd389f8273463
Manuel <mhummel@student.42heilbronn.de>
2025-01-09 12:37:45 +0100

    Create HumanB.cpp
    Implements HumanB's methods. Uses pointer to allow for optional weapon

---
+++ ex03/HumanB.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 11:38:25 by mhummel           #+#    #+#             */
+/*   Updated: 2025/01/09 12:37:31 by mhummel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HumanB.hpp"
+#include <iostream>
+
+HumanB::HumanB(const std::string &name) : name(name), weapon(NULL)
+{
+}
+
+void HumanB::setWeapon(Weapon &weapon)
+{
+	this->weapon = &weapon;
+}
+
+void HumanB::attack() const
+{
+	if (weapon)
+	{
+		std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+	}
+	else
+	{
+		std::cout << name << " has no weapon to attack with" << std::endl;
+	}
+}
