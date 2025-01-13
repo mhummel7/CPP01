57126196f6254e723facdff875e24b6244bf541e
Manuel <mhummel@student.42heilbronn.de>
2025-01-13 12:33:12 +0100

    Create main.cpp
    Runs the program, calls every function and then returns 0.

---
+++ ex05/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 11:32:22 by mhummel           #+#    #+#             */
+/*   Updated: 2025/01/13 12:30:47 by mhummel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	harl.complain("INVALID");
+
+	return 0;
+}
